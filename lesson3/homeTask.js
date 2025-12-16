0a239cae995adf2f9b94002fe3d0850109538aa3
skittles <65069112+platon453@users.noreply.github.com>
2025-12-17 00:20:43 +0300

    use prettier

--- lesson3/homeTask.js
+++ lesson3/homeTask.js
@@ -2,11 +2,10 @@ const pinCodes = ["1111", "5678", "1234", "9999"];
 const myPin = "1234";
 
 for (const pins of pinCodes) {
-    if (myPin === pins) {
-        console.log(`Ваш пинк код совпадает с ${myPin}`);
-        break;
-    } else {
-        console.log("Неверный пин!");
-    }
+  if (myPin === pins) {
+    console.log(`Ваш пинк код совпадает с ${myPin}`);
+    break;
+  } else {
+    console.log("Неверный пин!");
+  }
 }
-
