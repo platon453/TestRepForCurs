0b7f9db225e31a6882fbab388b8fc08a64993bb5
skittles <65069112+platon453@users.noreply.github.com>
2026-01-05 17:22:14 +0300

    homeTask5

---
+++ lesson5/zadanie2.js
@@ -0,0 +1,26 @@
+const sleep = (ms) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+};
+
+const cookDinner = async () => {
+    try {
+        console.log("Программа по готовке еды запущенна....");
+
+        await sleep(1000);
+        console.log("Включаю плиту...");
+
+        await sleep(2000);
+        console.log("Режу овощи...");
+
+        await sleep(3000);
+        console.log("Варю суп...");
+
+        console.log("Ужин готов");
+        console.log("Программа по готовке еды успешно завершилась");
+    } catch(error) {
+        console.log("Программа выполнилась с ошибкой", error);
+    }
+}
+cookDinner();
