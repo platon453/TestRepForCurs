e561ef4f656cd454204f58ed333650cc82833c84
skittles <platon.fedotov97@mail.ru>
2026-01-15 21:20:10 +0300

    Create index.js

---
+++ lesson9/index.js
@@ -0,0 +1,58 @@
+const http = require('node:http');
+
+
+const PORT = 3000;
+
+const server = http.createServer(async (request, response) => {
+    if (request.method === 'GET' && request.url === '/') {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.statusCode = 200;
+
+        response.end(`
+            <form action="/save" method="POST">
+                <input name="username" placeholder="Ваше имя" />
+                <button type="submit">Отправить</button>
+            </form>
+            `);
+    } else if (request.method === 'POST' && request.url === '/save') {
+        const body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk); // запихиваем chunk в конец массива body[]
+            console.log(`Прилетел кусочек данных: ${chunk}`);
+        });
+
+        request.on('end', () => {
+            // Соеденяет буферы + возращает обьед буфер
+            const parsedBody = Buffer.concat(body).toString('utf-8');
+
+            const decodedSting = decodeURIComponent(parsedBody);
+
+            console.log(`Полное тело запроса: ${decodedSting}`);
+
+            response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            response.statusCode = 200; 
+            
+            response.end(`Данные получены! Вы прислали ${decodedSting}`);
+            
+        });
+    } else if (request.method === 'GET' && (request.url === '/servers' || request.url === '/users')) {
+        const data = await fetch('https://gusic.xyz/stats');
+        const jsonData = await data.json(); // { servers: 123123, users:123121 }
+
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.statusCode = 200;
+
+        if (request.url === '/servers') {
+            response.end(`<h1>Количество серверов на данный момент: ${jsonData.servers}</h1>`);
+        } else if (request.url === '/users') {
+            response.end(`<h1>Количество пользователей на данный момент: ${jsonData.users}</h1>`);
+        }
+        
+    } else if (request.method === 'GET' && request.url === '/weather') {
+
+    }
+});
+
+server.listen(PORT);
+console.log(`Server is already run on adress http://localhost:${PORT}`);
